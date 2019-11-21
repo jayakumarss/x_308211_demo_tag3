aeeddd71841657c7d051e9d1bc44d457e44f9c37
System Administrator <admin@example.com>
2019-11-21 16:43:44 -0500

    Changes by 'System Administrator' on 'Demo TAG3 - 1.0.0' -- Build #1

---
+++ sn/Demo TAG3/Dictionary Entry/x_308211_demo_tag3_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Demo TAG3
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-11-21 21:42:34
+ * Created By  : admin
+ * Updated On  : 2019-11-21 21:42:34
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=c1c2c54ddb154010343f58b3ca9619fa
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
