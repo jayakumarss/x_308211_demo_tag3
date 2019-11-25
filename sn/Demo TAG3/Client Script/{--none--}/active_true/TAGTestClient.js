1ba783060f98185e9c7b14e74eaaf63095b1a02c
System Administrator <admin@example.com>
2019-11-25 13:40:32 -0500

    Changes by 'System Administrator' on 'Demo TAG3 - 1.0.0' -- Build #14

---
+++ sn/Demo TAG3/Client Script/{--none--}/active_true/TAGTestClient.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Demo TAG3
+ * ClassName   : sys_script_client
+ * Created On  : 2019-11-25 18:37:32
+ * Created By  : admin
+ * Updated On  : 2019-11-25 18:37:32
+ * Updated By  : admin
+ * URL         : /sys_script_client.do?sys_id=c8be3cdedbd10010343f58b3ca9619b1
+ */
+alert('test');
---
+++ sn/Demo TAG3/Script Include/active_true/x_308211_demo_tag3.TAGTestScript.js
@@ -0,0 +1,28 @@
+/* 
+ * Application : Demo TAG3
+ * ClassName   : sys_script_include
+ * Created On  : 2019-11-25 18:36:46
+ * Created By  : admin
+ * Updated On  : 2019-11-25 18:36:49
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=338eb8dedbd10010343f58b3ca961970
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_308211_demo_tag3.module:sys_script_include
+ */
+var TAGTestScript = Class.create();
+TAGTestScript.prototype = /** @lends x_308211_demo_tag3.module:sys_script_include.TAGTestScript.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+
+    type: 'TAGTestScript'
+};
